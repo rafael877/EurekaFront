4a00e688beddbb486ec12edcb29433c87bf691e2
Rafael887 <rafaelnerisoficial@gmail.com>
2025-06-10 21:43:30 -0300

    tudo

---
+++ EurekaFrontEnd/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { HomePage } from './components/home-page/home-page';
+
+export const routes: Routes = [
+  {
+    path: '', component: HomePage
+    // path: 'detalhes', component: DetailPage
+    //coementario
+  }
+];
